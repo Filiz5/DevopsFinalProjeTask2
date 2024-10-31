9a66c062c62b0f98f1435e7a4868ab339828081f
Melis Filiz Esenkaya <127357851+Filiz5@users.noreply.github.com>
2024-10-31 17:16:18 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,13 +1,3 @@
 output "public_ip" {
     value = aws_instance.devops_project_instance[*].public_ip
 }
-
-output "workspace_instance_ip" {
-  description = "Public IP"
-  value       = "${terraform.workspace}-instance ip: ${aws_instance.devops_project_instance.public_ip}"
-}
-
-output "instance_id" {
-  description = "Public Id"
-  value       = aws_instance.devops_project_instance.id
-}
